d06624b855d4d19b1adf427c078ece5d84cdfd5d
SeniorPickle <152529503+SeniorPickle@users.noreply.github.com>
2023-12-08 15:22:37 -0600

    update

--- Endless Wandering/EyeSeer.gd
+++ Endless Wandering/EyeSeer.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 80.0
 const JUMP_VELOCITY = -400.0
+var dir = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -18,9 +19,11 @@ func _physics_process(delta):
 		$AnimationPlayer.play("moving_towards")
 		$AnimatedSprite2D.flip_h = true
 		velocity.x = SPEED*-1
+		dir = 1
 		
 	elif $RightCheck.overlaps_body(CharacterLoader.get_node("Player")) and not edge:
 		velocity.x = SPEED*1
+		dir = -1
 		$AnimationPlayer.play("moving_towards")
 		$AnimatedSprite2D.flip_h = false
 	else:
@@ -39,3 +42,4 @@ func _physics_process(delta):
 func _on_area_2d_body_entered(body):
 	if body == CharacterLoader.get_node("Player"):
 		CharacterLoader.get_node("Player").health -= 10
+		position.x += 40*dir
